d3e6e35e0a61a6f3f5bd444266b5cc725ce4ac01
mek <mwolde617@gmail.com>
2024-03-28 19:07:29 +0000

    import-ec2-updated

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "imported-instance" {
+resource "aws_instance" "main-account-ec2" {
  ami           = "ami-0c101f26f147fa7fd"
  instance_type = "t2.micro"
 
@@ -15,10 +15,10 @@ resource "aws_instance" "imported-instance" {
   */
 }
 
-resource "aws_instance" "imported-instance1" {
+resource "aws_instance" "aws-test-ec2" {
  provider = aws.awstest
  ami           = "ami-06e46074ae430fba6"
- instance_type = "t2.small"
+ instance_type = "t2.micro"
 
  tags = {
    "Name1": "import1"
